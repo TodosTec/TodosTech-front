9808cad12166d686d8f7ab180d7f05a846740e47
Guilherme Felipe Luz Martins <guilhermeluz-ieg@instituto.ad>
2023-11-07 18:30:25 -0300

    alteração de api de noticias

--- src/pages/Noticia/index.jsx
+++ src/pages/Noticia/index.jsx
@@ -17,12 +17,11 @@ export function Noticia() {
     function getNoticias() {
         let temas = ['lgbt', 'gay', 'lesbian', 'bisexual', 'transgender']
         let valorPageSize = 20
-        for (let i = 0; i < temas.length; i++) {
             axios({
                 method: "get",
                 url: "https://gnews.io/api/v4/search?q=example&apikey=6a057afdf7b0a712badd9ab73761d017",
                 params: {
-                    q: temas[i],
+                    q: 'lgbt',
                     to: new Date().toISOString().slice(0, 10),
                     from: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10),
                     // qtd: valorPageSize,
@@ -38,28 +37,12 @@ export function Noticia() {
                         // console.log(response.data.articles[j]);
                         arrayNoticias.push(response.data.articles[j])
                     }
-                    // console.log(arrayNoticias);
-                    if (arrayNoticias.length >= 20) {
-                        i = 6
-                        for (let i = 0; i < arrayNoticias.length; i++) {
-                            if (arrayNoticias[i].urlToImage && arrayNoticias[i].title && arrayNoticias[i].author) {
-                                setObjectFirstNoticia(arrayNoticias[i]);
-                                break; // Sai do loop quando encontra a primeira imagem de URL válida
-                            }
-                        }
-                        // console.log(arrayNoticias.splice(20))
-                        // console.log(arrayNoticias[20]);
-                        // console.log(arrayNoticias.length - 20);
-
-                    }
-                    else {
-                        valorPageSize -= response.data.length
-                    }
+                    setObjectFirstNoticia(response.data.articles[0])
                 })
                 .catch((error) => {
                     console.log(error);
                 })
-        }
+                console.log(arrayNoticias);
     }
     useEffect(() => {
         getNoticias()
@@ -77,22 +60,22 @@ export function Noticia() {
       }, []);
     return (
         <div className="Noticia">
-            <img src={logo} alt="" />
+            <img src={logo} alt="" onClick={() => {console.log(arrayNoticias);}}/>
             <div className="container">
-                <div className="top" style={{ backgroundImage: `url(${objectFirstNoticia.urlToImage})` }}>
+                <div className="top" style={{ backgroundImage: `url(${objectFirstNoticia.image})` }}>
                     <h1 className="titulo">{objectFirstNoticia.title}</h1>
                     <p>{objectFirstNoticia.author}</p>
                 </div>
                 <div className="noticiasDesc">
                     {arrayNoticias.map((noticia, index) => (
                         <NoticiaComponente key={index}
-                            autor={noticia.author}
                             titulo={noticia.title}
+                            autor={noticia.source.name}
                             data={new Date(noticia.publishedAt).toLocaleDateString('pt-BR')}
                             aoClicar={() => {
                                 setUrlNoticiaAtomValue(noticia.url);
                                 navigate('/noticiaWebView');
-                              }}
+                            }}
                         />
                     ))}
                 </div>
